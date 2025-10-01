50564cc2913549de47efb0d95499b27fceb348dc
Sunrise <ieg-v@mail.ru>
2025-10-01 18:58:35 +0700

    feat: add homepage view and routing

--- posts/views.py
+++ posts/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Добро пожаловать не туда!</h1>")
