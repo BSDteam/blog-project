000fc379ec5a504117f73b6ce7ffd230b0e5bf70
Sunrise <ieg-v@mail.ru>
2025-10-01 18:38:31 +0700

    feat: initial Django project setup

---
+++ posts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+
+def home_view(request):
+    return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
